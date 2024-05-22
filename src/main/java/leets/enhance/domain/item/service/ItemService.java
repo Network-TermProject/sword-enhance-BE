6f97d63ae64fee86f85eaa3171b6104af0b21c00
koungq <guu119@naver.com>
2024-05-22 17:09:58 +0900

    FEAT: 강화 및 아이템 조회 메서드 추가

--- src/main/java/leets/enhance/domain/item/service/ItemService.java
+++ src/main/java/leets/enhance/domain/item/service/ItemService.java
@@ -1,10 +1,15 @@
 package leets.enhance.domain.item.service;
 
+import jakarta.transaction.Transactional;
+import leets.enhance.domain.item.entity.DestroyProbability;
 import leets.enhance.domain.item.entity.Item;
+import leets.enhance.domain.item.entity.Status;
+import leets.enhance.domain.item.entity.SuccessProbability;
 import leets.enhance.domain.item.model.request.ItemRegisterDto;
 import leets.enhance.domain.item.repository.ItemRepository;
 import leets.enhance.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,15 +20,38 @@ public class ItemService {
 
     private final ItemRepository itemRepository;
 
+    @Transactional
     public Item register(User user, ItemRegisterDto requestDto) {
         Item item = Item.of()
                 .name(requestDto.getName())
                 .build();
 
+        user.addItem(item);
+
         return itemRepository.save(item);
     }
 
     public List<Item> findTop10() {
-        return itemRepository.findTop10ByIsBrokenOrderByLevel(false);
+        return itemRepository.findTop10ByIsBrokenOrderByLevelDesc(false);
+    }
+
+    @Transactional
+    public Status enhance(Item item) {
+        if(item.getIsBroken()) {
+            throw new RuntimeException("이미 파괴된 검입니다."); // 수정: 커스텀 예외로 처리
+        }
+
+        if(Math.random() <= SuccessProbability.getProbability(item.getLevel())) {   // 강화 성공
+            return item.success();
+        } else {    // 실패 시
+            if(Math.random() <= DestroyProbability.getProbability(item.getLevel())) // 파괴 성공
+                return item.destroy();
+            return item.fail(); // 파괴 실패
+        }
+    }
+
+    public Item getItem(Long itemId) {
+        return itemRepository.findById(itemId)
+                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 검입니다."));
     }
 }
