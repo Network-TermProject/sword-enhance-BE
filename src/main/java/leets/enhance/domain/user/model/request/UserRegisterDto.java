1616c146461d4cbeaf5e0afb3863ba62d208d839
koungq <guu119@naver.com>
2024-05-22 17:08:31 +0900

    CHORE: 불필요한 엔터 삭제

--- src/main/java/leets/enhance/domain/user/model/request/UserRegisterDto.java
+++ src/main/java/leets/enhance/domain/user/model/request/UserRegisterDto.java
@@ -3,10 +3,10 @@ package leets.enhance.domain.user.model.request;
 import lombok.Getter;
 
 @Getter
-public class UserRegisterDto {
-
-    private String email;
+public class UserRegisterDto {  // 수정: DTO 모두 class -> record
+    private String email;   // 수정: Validation 추가
     private String name;
     private String pwd;
     private String checkPwd;
+    private Boolean isChecked;
 }
