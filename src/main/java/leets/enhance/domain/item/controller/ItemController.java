f35e9e4dc3d5038e1195e2617582976ef9a36fce
koungq <guu119@naver.com>
2024-05-22 17:06:30 +0900

    FIX: 잘못된 RequestMapping 수정

--- src/main/java/leets/enhance/domain/item/controller/ItemController.java
+++ src/main/java/leets/enhance/domain/item/controller/ItemController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/items")
 @RequiredArgsConstructor
 public class ItemController {
 
